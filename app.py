e905077393d3030eab9e8424d0a39c57dddd09e4
Ellen <ellen@ovillavet.com>
2017-11-19 13:25:26 -0600

    landing page added

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import magical_object
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/searchtracks')
 def searchtracks():
     return render_template('searchtracks.html')
